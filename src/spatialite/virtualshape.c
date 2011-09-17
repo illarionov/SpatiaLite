5a822343499032d184f5d3f87be14213cb912a25
bradh@bb1f2614-3cb5-4af3-869d-d069165d98ce <bradh@bb1f2614-3cb5-4af3-869d-d069165d98ce>
2011-09-17 10:53:53 +0000

    Fix handling of non-equal comparison for text entries in virtualshape
    This shows up if you have a case like:
    SELECT ... FROM virtualshapetable WHERE textcol > "something";
    (or less than / greater than or equal to / or less than or equal to).
    
    Its a subtle issue with the strcmp function. It turns out that it
    doesn't always return -1 / 0 / 1. Sometimes it returns 7. Or other
    integer values. Always negative int / 0 / positive int for less than,
    equal to / greater than.

--- src/spatialite/virtualshape.c
+++ src/spatialite/virtualshape.c
@@ -661,19 +661,19 @@ vshp_eval_constraints (VirtualShapeCursorPtr cursor)
 						  ok = 1;
 					      break;
 					  case SQLITE_INDEX_CONSTRAINT_GT:
-					      if (ret == 1)
+					      if (ret > 0)
 						  ok = 1;
 					      break;
 					  case SQLITE_INDEX_CONSTRAINT_LE:
-					      if (ret == -1 || ret == 0)
+					      if (ret <= 0)
 						  ok = 1;
 					      break;
 					  case SQLITE_INDEX_CONSTRAINT_LT:
-					      if (ret == -1)
+					      if (ret < 0)
 						  ok = 1;
 					      break;
 					  case SQLITE_INDEX_CONSTRAINT_GE:
-					      if (ret == 1 || ret == 0)
+					      if (ret >= 0)
 						  ok = 1;
 					      break;
 					  };
