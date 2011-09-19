06828944bb202bf5da0a220218b4fbb7d9687fdb
bradh@bb1f2614-3cb5-4af3-869d-d069165d98ce <bradh@bb1f2614-3cb5-4af3-869d-d069165d98ce>
2011-09-19 12:30:07 +0000

    Fix string comparison operators for VirtualXL
    Affects less than, less than or equal to, greater than, greater than or equal to.

--- src/spatialite/virtualXL.c
+++ src/spatialite/virtualXL.c
@@ -663,19 +663,19 @@ vXL_eval_constraints (VirtualXLCursorPtr cursor)
 			  ok = 1;
 		      break;
 		  case SQLITE_INDEX_CONSTRAINT_GT:
-		      if (ret == 1)
+		      if (ret > 0)
 			  ok = 1;
 		      break;
 		  case SQLITE_INDEX_CONSTRAINT_LE:
-		      if (ret == -1 || ret == 0)
+		      if (ret <= 0)
 			  ok = 1;
 		      break;
 		  case SQLITE_INDEX_CONSTRAINT_LT:
-		      if (ret == -1)
+		      if (ret < 0)
 			  ok = 1;
 		      break;
 		  case SQLITE_INDEX_CONSTRAINT_GE:
-		      if (ret == 1 || ret == 0)
+		      if (ret >= 0)
 			  ok = 1;
 		      break;
 		  };
