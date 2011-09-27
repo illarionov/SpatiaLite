23b636a47e3583489749ba6e0920972e22db861a
bradh@bb1f2614-3cb5-4af3-869d-d069165d98ce <bradh@bb1f2614-3cb5-4af3-869d-d069165d98ce>
2011-09-27 23:22:59 +0000

    Update contributor list for recent patch by Ahmadou Dicko

--- test/shape_utf8_2.c
+++ test/shape_utf8_2.c
@@ -27,6 +27,7 @@ the Initial Developer. All Rights Reserved.
 
 Contributor(s):
 Brad Hards <bradh@frogmouth.net>
+Ahmadou Dicko <dicko.ahmadou@gmail.com>
 
 Alternatively, the contents of this file may be used under the terms of
 either the GNU General Public License Version 2 or later (the "GPL"), or
